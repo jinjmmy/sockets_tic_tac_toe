7bacddd4483e3ad4ebfb24ad500bb7e9c44b4563
Jimmy Jin <114312503+jinjmmy@users.noreply.github.com>
2023-08-01 18:40:41 -0700

    Spacing out the results better

--- player1GUI.py
+++ player1GUI.py
@@ -1,6 +1,7 @@
 """This is client (player 1)."""
 import tkinter as tk
 from gameboard import BoardClass
+from tkinter import font
 import socket
 
 
@@ -231,35 +232,39 @@ class GUI:
         for wid in lists:
             wid.destroy()
 
-
     def displayStats(self):
         """Prints the overall stats for the player."""
         self.newSocket.send('stat'.encode())
 
         self.clearWidgets()
 
-        self.title = tk.Label(self.root, text='GAME STATS')
-        self.title.grid(row=0, column=1)
+        # Set up font
+        title_font = font.Font(family='Helvetica', size=18, weight='bold')
+        label_font = font.Font(family='Helvetica', size=12)
 
-        self.nameLabel = tk.Label(self.root, text=f'Player : {self.gameBoard.playerName}')
-        self.nameLabel.grid(row=2, column=1)
+        # Set up title label
+        self.title = tk.Label(self.root, text='GAME STATS', font=title_font)
+        self.title.grid(row=0, column=1, padx=10, pady=10, sticky='n')
 
-        self.oppLabel = tk.Label(self.root, text=f'Opponent : {self.gameBoard.player2name}')
-        self.oppLabel.grid(row=4, column=1)
+        # Set up other labels
+        self.nameLabel = tk.Label(self.root, text=f'Player: {self.gameBoard.playerName}', font=label_font)
+        self.nameLabel.grid(row=2, column=1, padx=10, pady=5, sticky='n')
 
-        self.winLabel = tk.Label(self.root, text=f'Number of Wins : {self.gameBoard.p1wins}')
-        self.winLabel.grid(row=6, column=1)
+        self.oppLabel = tk.Label(self.root, text=f'Opponent: {self.gameBoard.player2name}', font=label_font)
+        self.oppLabel.grid(row=4, column=1, padx=10, pady=5, sticky='n')
 
+        self.winLabel = tk.Label(self.root, text=f'Number of Wins: {self.gameBoard.p1wins}', font=label_font)
+        self.winLabel.grid(row=6, column=1, padx=10, pady=5, sticky='n')
 
-        self.lossLabel = tk.Label(self.root, text=f'Number of Losses : {self.gameBoard.p1loss}')
-        self.lossLabel.grid(row=8, column=1)
+        self.lossLabel = tk.Label(self.root, text=f'Number of Losses: {self.gameBoard.p1loss}', font=label_font)
+        self.lossLabel.grid(row=8, column=1, padx=10, pady=5, sticky='n')
 
-        self.tieLabel = tk.Label(self.root, text=f'Number of Ties : {self.gameBoard.ties}')
-        self.tieLabel.grid(row=10, column=1)
+        self.tieLabel = tk.Label(self.root, text=f'Number of Ties: {self.gameBoard.ties}', font=label_font)
+        self.tieLabel.grid(row=10, column=1, padx=10, pady=5, sticky='n')
 
-        self.totalLabel = tk.Label(self.root, text=f'Total Games Played : {self.gameBoard.updateGamesPlayed()}')
-        self.totalLabel.grid(row=12, column=1)
-
+        self.totalLabel = tk.Label(self.root, text=f'Total Games Played: {self.gameBoard.updateGamesPlayed()}',
+                                   font=label_font)
+        self.totalLabel.grid(row=12, column=1, padx=10, pady=5, sticky='n')
 
     def convertPostion(self, button):
         if button == self.b0:
